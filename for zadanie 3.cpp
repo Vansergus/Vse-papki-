e1c572b76271348d250d35786adccc2152e3f507
Vansergus <56320602+Vansergus@users.noreply.github.com>
2019-10-15 22:16:20 +0500

    first commit

---
+++ for zadanie 3.cpp
@@ -0,0 +1,16 @@
+﻿
+#include <iostream>
+#include <string>
+#include <math.h>
+
+
+int main()
+{
+	int z, n;
+	std::cout << "vedite cislo: ";
+	std::cin >> n;
+	for (int i = 0; i <= n; i++) {
+		std::cout << i << ",";
+	}
+}
+
